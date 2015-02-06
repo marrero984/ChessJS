32995e8cf3ca3fd4e7fe78902efc82ab66eadb37
Michael J. Marrero <michael.j.marrero@outlook.com>
2015-02-06 13:39:46 -0800

    moved alternating board color logic to css.  now creates all peices and assigns init positions.

--- chess.js
+++ chess.js
@@ -1,17 +1,54 @@
-// No need to track number of moves in object; just count length of history
-// Create each piece on initialization, setting vertical, horizontal, and side
 // Black pawns always move down, white pawns always up
-// Transform function changes piece type
 
 // The grid is available to the whole script and stores only the individual cells
 // The board returned by createBoard() is the full set of elements needed for display and used only on initialization
-var grid = [];
+var grid = []; // Must be initialized before anonymous function call
+var pieces= {};
+var sides = ['white','black'];
+var ranks = {
+	'Pawn': {
+		'count': 8,
+		'elementText': 'P',
+		'initHorizontal': [0, 1, 2, 3, 4, 5, 6, 7]
+	},
+	'Bishop': {
+		'count': 2,
+		'elementText': 'B',
+		'initHorizontal': [2, 5]
+	},
+	'Knight': {
+		'count': 2,
+		'elementText': 'Kn',
+		'initHorizontal': [1, 6]
+	},
+	'Rook': {
+		'count': 2,
+		'elementText': 'R',
+		'initHorizontal': [0, 7]
+	},
+	'Queen': {
+		'count': 1,
+		'elementText': 'Q',
+		'initHorizontal': [4]
+	},
+	'King': {
+		'count': 1,
+		'elementText': 'K',
+		'initHorizontal': [3]
+	}
+};
 
 (function () {	
 	document.getElementById('board').appendChild(Board());
+	createPieces();
 
 	// Tests TODO: remove
-	tests();
+	console.log(pieces);
+	console.log(pieces['whitePawn1']);
+	pieces['whitePawn1'].display();
+	for (var piece in pieces) {
+		pieces[piece].display();
+	}
 })();
 
 function Board () {
@@ -20,14 +57,8 @@ function Board () {
 	for (var i = 0; i < 8; i++) {
 		var row = document.createElement('div'),
 			gridRow = [];
-		row.setAttribute('class','row')
 		for (var j = 0; j < 8; j++) {
 			var cell = document.createElement('div');
-			if (i % 2 === j % 2) {
-				cell.setAttribute('class','cell alt');
-			} else {
-				cell.setAttribute('class', 'cell');
-			}
 			row.appendChild(cell);
 			gridRow.push(cell);
 		}
@@ -58,44 +89,53 @@ function Piece (side, rank) {
 	this.upgrade = function (newRank) {
 		if (this.rank === 'pawn') {
 			this.rank = newRank;
+			this.element = pieceElement(this.side, this.rank);
+			this.display();
 		} else {
 			console.log('Can only upgrade pawns');
 		}
 	}
-}
 
-// See if possible to move this function to inside the Piece object
-function pieceElement (side, rank) {
-	var e = document.createElement('div');
-	e.setAttribute('class','piece ' + side);
-	e.innerHTML = 'P'
-	return e;
-}
-
-//Returns pieces object that contains 32 Piece objest
-function createPieces () {
-	var pieces = {};
-	var sides = ['white','black'];
-	var ranks = {
-		'Pawn': 8,
-		'Bishop': 2,
-		'Knight': 2,
-		'Rook': 2,
-		'Queen': 1,
-		'King': 1
-	};
-
-	for (var rank in ranks) {
-		creationLoop(rank, ranks[rank]);
+	//Does not need to be accessible outside of the object
+	function pieceElement (side, rank) {
+		var e = document.createElement('div');
+		e.setAttribute('class','piece ' + side);
+		e.innerHTML = ranks[rank].elementText;
+		return e;
 	}
+}
 
-	function creationLoop (rank, x) {
+// Returns pieces object that contains 32 Piece objects
+// See if possible to move pieces into Board object
+function createPieces () {
+	for (var rank in ranks) {
 		for (var i = 0; i < 2; i++) {
 			var side = sides[i];
-			for (var j = 1; j <= x; j++) {
-				pieces[side + rank + j] = new Piece(side, rank);
+			for (var j = 1; j <= ranks[rank].count; j++) {
+				var name = side + rank + j;
+				pieces[name] = new Piece(side, rank);
+				pieces[name].vertical = initVertical(side, rank);
+				pieces[name].horizontal = ranks[rank].initHorizontal[j - 1];
 			}
 		}
+	}
+
+	function initVertical (side, rank) {
+		var v;
+		if (rank === 'Pawn') {
+			if (side === 'white') {
+				v = 6;
+			} else {
+				v = 1;
+			}
+		} else {
+			if (side === 'white') {
+				v = 7;
+			} else {
+				v = 0;
+			}
+		}
+		return v;
 	}
 
 	return pieces;
@@ -108,9 +148,4 @@ function inRange (i) {
 	} else {
 		return false;
 	}
-}
-
-// Tests
-function tests () {
-	console.log(createPieces());
 }
